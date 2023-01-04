c6fd8e6d64df1fc6b099a38934edd72b0958c622
Nabizachi <georgiy.bobrov@mail.ru>
2023-01-04 22:18:17 +0700

    кнопка не работает(

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import MyButton from "./UI/MyButton";
+import './styles/App.css'
 
 function App() {
   return (
     <div >
-
-    </div>
+      <MyButton />
+    </div >
   )
 }
 
---
+++ src/UI/MyButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from './MyButton.style.css'
+
+
+const MyButton = () => {
+    return (
+        <button className={classes.Btn}>
+
+        </button>
+    )
+}
+
+export default MyButton
