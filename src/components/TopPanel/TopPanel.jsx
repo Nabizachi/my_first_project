5d815cec25771b83f594b851631f2b55ae3d3087
Nabizachi <georgiy.bobrov@mail.ru>
2023-01-05 15:54:41 +0700

    fix panel

--- src/components/TopPanel/TopPanel.jsx
+++ src/components/TopPanel/TopPanel.jsx
@@ -6,27 +6,27 @@ import Logo from '../../Images/logo.png'
 
 const TopPanel = () => {
     return (
-        <header className="header">
+        <div className="header">
             <div className="logoBox">
                 <div className="logoImage">
                     <img src={Logo} alt='/' />
                 </div>
 
-
                 <div className="logoText">
-                    <h1 > Конструкторский отдел</h1>
+                    <h1 > Конструкторский</h1>
+                    <h1> отдел</h1>
                 </div>
             </div>
 
-            <div>
-                <ul className="menu">
-                    <li><MyButton>Домой</MyButton></li>
-                    <li><MyButton>В работе</MyButton></li>
-                    <li><MyButton>Дополнительно</MyButton></li>
-                    <li><MyButton>Решенные</MyButton></li>
-                </ul>
+            <div className="menu">
+
+                <MyButton>Домой</MyButton>
+                <MyButton>В работе</MyButton>
+                <MyButton>Дополнительно</MyButton>
+                <MyButton>Решенные</MyButton>
+
             </div>
-        </header >
+        </div >
     )
 }
 
