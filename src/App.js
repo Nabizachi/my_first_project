9fc9db79106080c55a0fd05e9cae7509f792fb9b
Nabizachi <georgiy.bobrov@mail.ru>
2023-01-07 01:35:16 +0700

    top panel fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React from "react";
+import React from "react"
 import './styles/App.css'
-import TopPanel from "./components/TopPanel/TopPanel";
-import TopLogoPanel from "./components/TopLogoPanel/TopLogoPanel";
+import TopLogoPanel from "./components/TopLogoPanel/TopLogoPanel"
 
 function App() {
   return (
     <div >
       <TopLogoPanel />
-      <TopPanel />
     </div >
   )
 }
--- src/components/TopLogoPanel/TopLogoPanel.jsx
+++ src/components/TopLogoPanel/TopLogoPanel.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import './TopLogoPanel.css'
 import Logo from '../../Images/logo.png'
+import MyButton from '../../UI/MyButton/MyButton'
 
 
 const TopLogoPanel = () => {
     return (
         <div className="logoPanel">
-            <div className='logoPanelImage'>
-                <img className="logoImage" src={Logo} alt='/' />
+            <div className="imageButtonBox">
+                <div className='logoPanelImage'>
+                    <img className="logoImage" src={Logo} alt='/' />
+                </div>
+                <div className="buttonBox">
+                    <MyButton>Проекты</MyButton>
+                    <MyButton>Актуальное КД</MyButton>
+                    <MyButton>Склад</MyButton>
+                </div>
             </div>
             <div className="logoPanelText">
                 <p>Портал конструкторского отдела</p>
