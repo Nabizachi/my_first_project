f2ef784c4a7515ae81d0b8b15edee5bf447d34d2
Nabizachi <georgiy.bobrov@mail.ru>
2023-01-13 00:52:13 +0700

    redux probuy

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux';
 import App from './App';
 
+const defaultState = {
+  cash: 0,
+}
+
+
+// action = {type: '', payload: '?'}
+
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case 'ADD_CASH':
+      return { ...state, cash: state.cash + action.payload }
+
+    case "GET_CASH":
+      return { ...state, cash: state.cash - action.payload }
+
+    default:
+      return state
+  }
+
+}
+
+const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+
   </React.StrictMode>
 );
